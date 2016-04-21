884f5a25f6b95807c3d9bfc69f6a9e7fa2843049
tomharrisonjr <tom.harrison.jr@gmail.com>
2016-04-20 22:24:07 -0400

    Commented checkForWinner, found and fixed bug, simplified NoResult check

--- src/main/scala/com/tomharrisonjr/TicTacToeBoard.scala
+++ src/main/scala/com/tomharrisonjr/TicTacToeBoard.scala
@@ -5,18 +5,21 @@ package com.tomharrisonjr
   */
 class TicTacToeBoard(board: Array[Array[Player]]) {
 
-  // for tests, set up a board using strings of X's and O's
+  // constructor for tests, set up a board using strings of X's and O's
   def this(stringBoard: Array[String]) = this(stringBoard.map(row => TicTacToeBoard.getPlayersFromString(row)))
 
-  // allows for non-square boards?
+  // constructor allows for non-square boards?
   def this(rows: Int, cols: Int) = this(Array.fill(rows, cols)(Blank): Array[Array[Player]])
 
-  // square board
+  // square board constructor
   def this(size: Int) = this(size, size)
 
   val rowCount = board.length
+  // nicer Scala ternary, vs Java board.isEmpty ? 0 : board(0).length
   val columnCount = if (board.isEmpty) 0 else board(0).length
-  val columnNameMapping = (0 until 5 * columnCount).map(n => (TicTacToeBoard.numToAlpha(n), n)).toMap
+  // exclusive range 0 until n
+  // then, for each instance n apply the companion class function numToAlpha creating a collection of Tuples, converted toMap
+  val columnNameMapping = (0 until columnCount).map(n => (TicTacToeBoard.numToAlpha(n), n)).toMap
 
   val numInARowNeeded = 3
 
@@ -65,7 +68,7 @@ class TicTacToeBoard(board: Array[Array[Player]]) {
     diagonalsLTR foreach checkForWinner
     diagonalsRTL foreach checkForWinner
 
-    if (board.map(row => row.contains(Blank)).contains(true)) {
+    if (board.exists(row => row.contains(Blank))) {
       return GameResult.NoResult
     }
 
